e7041c067300672f6db1df5af9dcbf3fcfea3c5c
dwmoeller1 <dwmoeller1@aol.com>
2019-02-07 23:42:01 -0600

    Add project files.

---
+++ ParsingExamples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ParsingExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
